221f3c6df006ed388865d3442b325ce719408adc
Bob Kersten <info@bobkersten.nl>
2019-09-09 08:09:07 +0200

    first raw overview of timers and stopwatches in settings

---
+++ api.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Timer = require('./lib/timer.js');
+const Stopwatch = require('./lib/stopwatch.js');
+
+module.exports = [
+	{
+		method: 'GET',
+		path: '/timers/',
+		public: true,
+		fn: function(args, callback) {
+			let result = [];
+			for (var timerId in Timer.timers) {
+				let timer = Timer.get(timerId);
+				result.push({
+					id: timer.getId(),
+					name: timer.getName(),
+					duration: timer.getDuration()
+				});
+			}
+			callback(null, JSON.stringify(result));
+		}
+	},
+	{
+		method: 'DELETE',
+		path: '/timers/:id',
+		fn: function(args, callback) {
+			console.log(args.params.id);
+		}
+	},
+	{
+		method: 'GET',
+		path: '/stopwatches/',
+		fn: function(args, callback) {
+			let result = [];
+			for (var stopwatchId in Stopwatch.stopwatches) {
+				let stopwatch = Stopwatch.get(stopwatchId);
+				result.push({
+					id: stopwatch.getId(),
+					name: stopwatch.getName(),
+					duration: stopwatch.getDuration()
+				});
+			}
+			callback(null, JSON.stringify(result));
+		}
+	},
+	{
+		method: 'DELETE',
+		path: '/stopwatches/:id',
+		fn: function(args, callback) {
+			console.log(args.params.id);
+		}
+	}
+];
